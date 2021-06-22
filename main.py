994323932dbda3ed7ca8363d8badc49349db77c0
MustufaJaved-ElectronicEngineer <81430338+MustufaJaved-ElectronicEngineer@users.noreply.github.com>
2021-06-22 19:49:04 +0530

    This is Mustufa Javed Qureshi completed the project for cyber security project.

---
+++ main.py
@@ -0,0 +1,26 @@
+import requests
+#import os
+from datetime import datetime
+
+api_key = '4d70c7b61b32ea5a2125276269322580'
+location = input("Hi! Geek please give me your city name: ")
+
+complete_api_link = "https://api.openweathermap.org/data/2.5/weather?q="+location+"&appid="+api_key
+api_link = requests.get(complete_api_link)
+api_data = api_link.json()
+
+#create variables to store and display data
+temp_city = ((api_data['main']['temp']) - 273.15)
+weather_desc = api_data['weather'][0]['description']
+hmdt = api_data['main']['humidity']
+wind_spd = api_data['wind']['speed']
+date_time = datetime.now().strftime("%d %b %Y | %I:%M:%S %p")
+
+print ("-------------------------------------------------------------")
+print ("Weather Stats for - {}  || {}".format(location.upper(), date_time))
+print ("-------------------------------------------------------------")
+
+print ("Current temperature is: {:.2f} deg C".format(temp_city))
+print ("Current weather desc  :",weather_desc)
+print ("Current Humidity      :",hmdt, '%')
+print ("Current wind speed    :",wind_spd ,'kmph')
